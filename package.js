268ab1a2f5a2a5a66d13f6844dacd8966aa1b13c
tinque <quentin.georget@gmail.com>
2015-05-27 09:30:03 +0000

    modify version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tinque:angular-translate',
-  version: '2.7.0.1',
+  version: '2.7.0_1',
   summary: 'This is the Meteor package for angular-translate',
   git:'https://github.com/tinque/meteor-angular-translate.git',
   documentation: 'README.md'
